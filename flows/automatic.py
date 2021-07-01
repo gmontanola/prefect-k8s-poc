35f8cb6623040f6b258602ea260f13132b4efeab
Gabriel Montañola <gabriel@montanola.com>
2021-07-01 14:38:46 -0300

    feat(flows): add gh actions test flow (#7)

---
+++ flows/automatic.py
@@ -0,0 +1,17 @@
+from prefect import Flow, task
+from prefect.run_configs.kubernetes import KubernetesRun
+from prefect.storage.github import GitHub
+
+
+@task(name="Sucesso", log_stdout=True)
+def message() -> None:
+    print("Essa flow foi registrada com GitHub Actions")
+
+
+with Flow(
+    "automatic-registering",
+    storage=GitHub("gmontanola/prefect-k8s-poc", "/flows/automatic.py"),
+    run_config=KubernetesRun(),
+    labels=["k8s"],
+) as flow:
+    message()
