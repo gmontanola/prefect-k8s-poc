c2d1888370e45658d63428e51346ced4a86bcec8
Gabriel Montañola <gabriel@montanola.com>
2021-07-29 23:25:04 -0300

    test: check working docker cache

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -8,7 +8,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get -y upgrade
 apt-get -y install --no-install-recommends curl build-essential
-echo "Can we haz cache naow??/ I hope so..."
+echo "Can we haz cache naow??/ Trust the process."
 
 # Cleanup
 apt-get clean
