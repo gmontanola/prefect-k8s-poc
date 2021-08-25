c9fd542ee8502838f260c89a889303480a88b571
Gabriel Montañola <gabriel@montanola.com>
2021-08-25 20:16:52 -0300

    refactor: rename project folders

---
+++ flows/internal/echo.py
@@ -0,0 +1,19 @@
+from prefect import Flow, task
+from prefect.run_configs.kubernetes import KubernetesRun
+from prefect.storage.github import GitHub
+
+from prefect_shared_tasks.voice import scream
+
+
+@task(name="Hello World", log_stdout=True)
+def hello() -> None:
+    print("Olá mundo! Tá frio demais...")
+
+
+with Flow(
+    "hello-world",
+    storage=GitHub("gmontanola/prefect-k8s-poc", "/flows/echo.py"),
+    run_config=KubernetesRun(labels=["k8s"]),
+) as flow:
+    hello()
+    scream()
