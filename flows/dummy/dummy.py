a4220d459c1525618f2b66362031c57602db2c87
Gabriel Montañola <gabriel@montanola.com>
2021-08-25 22:05:23 -0300

    ci: add development workflow (#35)

---
+++ flows/dummy/dummy.py
@@ -0,0 +1,16 @@
+from prefect import Flow, task
+from prefect.run_configs.kubernetes import KubernetesRun
+from prefect.storage.github import GitHub
+
+
+@task(name="Hello World", log_stdout=True)
+def hello() -> None:
+    print("Olares")
+
+
+with Flow(
+    "dummy-2",
+    storage=GitHub("gmontanola/prefect-k8s-poc", "/flows/dummy/dummy.py"),
+    run_config=KubernetesRun(labels=["k8s"]),
+) as flow:
+    hello()
